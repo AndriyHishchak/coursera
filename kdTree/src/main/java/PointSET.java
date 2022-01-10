f64001ca953c50ad4494c66f1a53299d49554228
andriy hishchak <64685716+AndriyHishchak@users.noreply.github.com>
2022-01-10 13:16:54 +0200

    add the PointSet model and unimplemented methods

---
+++ kdTree/src/main/java/PointSET.java
@@ -0,0 +1,32 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+
+public class PointSET {
+
+    public Boolean isEmpty() {
+        return null;
+    }
+
+    public Integer size() {
+        return null;
+    }
+
+    public void insert(Point2D p) {
+    }
+
+    public Boolean contains(Point2D p) {
+        return null;
+    }
+
+    public void draw() {
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
+}
