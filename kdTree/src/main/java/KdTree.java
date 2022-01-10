237796deb5b1c360ae9b5198e7a9d9dd68aa16ca
andriy hishchak <64685716+AndriyHishchak@users.noreply.github.com>
2022-01-10 16:28:33 +0200

    implemented contains method

--- kdTree/src/main/java/KdTree.java
+++ kdTree/src/main/java/KdTree.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
-    private TreeNode root;
+    private static TreeNode root;
     private int size;
 
 
@@ -22,8 +22,9 @@ public class KdTree {
     public void insert(Point2D p) {
     }
 
-    public boolean contains(Point2D p) {
-        return false;
+    public boolean contains(TreeNode root, Point2D p) { // does the set contain point p?
+        checkNullToObject(p);
+        return contains(root, p);
     }
 
     public void draw() {
@@ -35,5 +36,10 @@ public class KdTree {
 
     public Point2D nearest(Point2D p) {
         return null;
+    }
+
+    private void checkNullToObject(Object object) {
+        if (object == null)
+            throw new IllegalArgumentException();
     }
 }
