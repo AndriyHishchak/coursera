9d5586069188daa44ded20d56869da6aeca637d0
andriy hishchak <64685716+AndriyHishchak@users.noreply.github.com>
2022-01-10 16:39:08 +0200

    implemented insert method

--- kdTree/src/main/java/KdTree.java
+++ kdTree/src/main/java/KdTree.java
@@ -2,9 +2,23 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
-    private static TreeNode root;
+    private TreeNode root;
     private int size;
 
+    private static class TreeNode {
+        private final Point2D point;
+        private TreeNode left, right;
+        private final RectHV rect;
+        private final boolean isSeparatedByX;
+
+        TreeNode(Point2D p, TreeNode left, TreeNode right, RectHV rect, boolean isSeparatedByX) {
+            this.point = p;
+            this.left = left;
+            this.right = right;
+            this.rect = rect;
+            this.isSeparatedByX = isSeparatedByX;
+        }
+    }
 
     public KdTree() {
         root = null;
@@ -20,11 +34,45 @@ public class KdTree {
     }
 
     public void insert(Point2D p) {
+        checkNullToObject(p);
+        if (isEmpty()) {
+            root = new TreeNode(p, null, null,
+                    new RectHV(null,null,null,null), true);
+            size = 1;
+            return;
+        }
+        TreeNode currNode = root, pre;
+        do {
+            if (currNode.point.equals(p))
+                return;
+            pre = currNode;
+            if ((currNode.isSeparatedByX && p.x() <= currNode.point.x()) ||
+                    (!currNode.isSeparatedByX && p.y() <= currNode.point.y()))
+                currNode = currNode.left;
+            else
+                currNode = currNode.right;
+
+        } while (currNode != null);
+        if (pre.isSeparatedByX) {
+            if (p.x() <= pre.point.x())
+                pre.left = new TreeNode(p, null, null,
+                        new RectHV(null, null, null, null), false);
+            else
+                pre.right = new TreeNode(p, null, null,
+                        new RectHV(null, null, null, null), false);
+        } else {
+            if (p.y() <= pre.point.y())
+                pre.left = new TreeNode(p, null, null,
+                        new RectHV(null, null, null, null), false);
+            else
+                pre.right = new TreeNode(p, null, null,
+                        new RectHV(null, null, null, null), false);
+        }
+        ++size;
     }
 
-    public boolean contains(TreeNode root, Point2D p) { // does the set contain point p?
-        checkNullToObject(p);
-        return contains(root, p);
+    public boolean contains(Point2D p) { // does the set contain point p?
+        return false;
     }
 
     public void draw() {
